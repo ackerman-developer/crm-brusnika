bda742ea028990a896c6e14d87cb122dd9b8383b
Artur Imankulov <arturimankulov906@gmail.com>
2023-12-29 20:46:52 +0500

    create table for lands-data

--- src/components/table/TableLand.tsx
+++ src/components/table/TableLand.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux-hooks';
 import { fetchLands } from '../../store/land-data/api-action';
+import styles from './TableLand.module.scss'
 
 
 export default function LandTable():JSX.Element {
@@ -12,20 +13,27 @@ export default function LandTable():JSX.Element {
   }, [dispatch])
 
   return (
-    <div>
-      <h2>Список земель:</h2>
+    <div className={styles.table}>
       <table>
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Название</th>
+            <th>Регистрационный номер</th>
+            <th>Адрес</th>
+            <th>Площадь объекта</th>
+            <th>Сведения о правообладателе</th>
+            <th>Объявленная стоимость</th>
+            <th>Канал поиска</th>
           </tr>
         </thead>
         <tbody>
         {lands.map((land) =>
           <tr key={land.id}>
-            <td>{land.id}</td>
             <td>{land.registerNumber}</td>
+            <td>{land.address}</td>
+            <td>{land.areaInMeters}</td>
+            <td>{land.aboutHolder}</td>
+            <td>{land.price}</td>
+            <td>{land.whoIsFound}</td>
           </tr>
         )}
         </tbody>
