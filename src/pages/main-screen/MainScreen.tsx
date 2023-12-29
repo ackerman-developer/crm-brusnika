65ca03e78577dc53e84dbd2cebe178648c8d48bb
Artur Imankulov <arturimankulov906@gmail.com>
2023-12-29 09:34:21 +0500

    create entity

--- src/pages/main-screen/MainScreen.tsx
+++ src/pages/main-screen/MainScreen.tsx
@@ -1,14 +1,17 @@
 import LandTable from '../../components/table/TableLand'
-import { useAppSelector } from '../../hooks/redux-hooks'
+import { useAppDispatch, useAppSelector } from '../../hooks/redux-hooks'
+import { fetchLands } from '../../store/land-data/api-action';
 import { getLands } from '../../store/land-data/land-data.selectors'
 import styles from './MainScreen.module.scss'
 
 export default function MainScreen():JSX.Element{
-  const handleClick = () =>{
-    console.log(lands)
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(fetchLands()); // Вызов асинхронного действия fetchLands при клике
   }
 
-  const lands = useAppSelector(getLands)
+  // const lands = useAppSelector(getLands)
 
   return(
     <div className={styles.container}>
